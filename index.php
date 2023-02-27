b1f6eec543eba187f4618d5669010d2ce10ce0ae
MIGU3L <99838970+miguelgustavo583@users.noreply.github.com>
2023-02-27 16:17:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php>
+
+$a = 0;
+$b = 10;
+
+if($b <= 10){
+    echo "O valor da variável b é maior ou igual a 10";
+}else{
+    echo "O valor da variárel b é menor que 10";
+}
+
+for($i = 0; $i <= 10; $i++){
+    echo $i;
+}
+
+$x = 0;
+while($x <= 10){
+    echo $x;
+    $x++;
+}
+
+$y = 0;
+do{
+    echo $y;
+    $y++;
+ 
+}while($y <= 10);
+
+?>
---
+++ pessoa.class.php
@@ -0,0 +1,81 @@
+<?php
+
+private $nome;
+private $endereco;
+private $bairro;
+private $cep;
+private $cidade;
+private $estado;
+
+
+public function getNome(){
+   return $this->nome;
+}
+
+public function setNome($nome){
+    $this->$nome;
+}
+
+
+
+
+
+
+public function getEndereco(){
+    return $this->endereco;
+ }
+
+ public function setEndereco($endereco){
+     $this->$endereco;
+ }
+
+
+
+
+
+ public function getBairro(){
+    return $this->bairro;
+ }
+ 
+ public function setBairro($bairro){
+     $this->$bairro;
+ }
+ 
+
+
+
+
+ public function getCep(){
+    return $this->nome;
+ }
+ 
+ public function setCep($cep){
+     $this->$cep;
+ }
+ 
+
+
+
+
+ public function getCidade(){
+    return $this->cidade;
+ }
+ 
+ public function setCidade($cidade){
+     $this->$cidade;
+ }
+ 
+
+
+
+
+ public function getEstado(){
+    return $this->estado;
+ }
+ 
+ public function setEstado($estado){
+     $this->$estado;
+ }
+ 
+
+?>
